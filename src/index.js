187d65991a8e6b4a24099f361c9917a98f2314a8
Fred <leferpz@gmail.com>
2024-05-28 14:40:44 +0100

    Added nav bar functionality and styling

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ let currentImage = imageList[index];
 let rightArrow = document.querySelector(".right-arrow");
 let leftArrow = document.querySelector(".left-arrow");
 
+// access the nav buttons
+let nav1 = document.querySelector("#nav1");
+let nav2 = document.querySelector("#nav2");
+let nav3 = document.querySelector("#nav3");
+let nav4 = document.querySelector("#nav4");
+let nav5 = document.querySelector("#nav5");
+let navList = [nav1, nav2, nav3, nav4, nav5];
+
 // make right arrow show next image
 rightArrow.addEventListener("click", () => {
     if (index === (imageList.length - 1)) {
@@ -38,6 +46,7 @@ function cycleRight() {
     currentImage.setAttribute("style", "display: block");
     console.log(currentImage);
     console.log(index);
+    updateNavBar();
 }
 
 function cycleLeft() {
@@ -48,6 +57,7 @@ function cycleLeft() {
     currentImage.setAttribute("style", "display: block");
     console.log(currentImage);
     console.log(index);
+    updateNavBar();
 }
 
 function cycleRightOverlap() {
@@ -58,6 +68,7 @@ function cycleRightOverlap() {
     currentImage.setAttribute("style", "display: block");
     console.log(currentImage);
     console.log(index);
+    updateNavBar();
 }
 
 function cycleLeftOverlap() {
@@ -68,7 +79,33 @@ function cycleLeftOverlap() {
     currentImage.setAttribute("style", "display: block");
     console.log(currentImage);
     console.log(index);
+    updateNavBar();
 }
+
+function updateNavBar() {
+    for (let i = 0; i < 5; i++) {
+        if (index === i) {
+            navList[i].setAttribute("style", "color: white");
+        } else {
+            navList[i].setAttribute("style", "color: black");
+        }
+    }
+}
+
+function updateImage() {
+    currentImage.setAttribute("style", "display: none");
+    currentImage = imageList[index];
+    currentImage.setAttribute("style", "display: block");
+}
+
+for (let i = 0; i < 5; i++) {
+    navList[i].addEventListener("click", () => {
+        index = i;
+        updateImage();
+        updateNavBar();
+    })}
+
+
 
 console.log(imageList);
 console.log(currentImage);
